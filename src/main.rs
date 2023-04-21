88f62e301dfbca4a1f27b5f474395d53bf9d833e
Bryn Deering <bdeering@uoguelph.ca>
2023-04-21 00:06:09 -0400

    Added last modification time + corrected dir size calculation

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ mod project_type;
 
 fn main() {
     let args = args::PolykillArgs::parse();
-
+    
+    println!("Searching for projects...");
     let projects = search::find_git_projects(Path::new(args.dir.as_str()));
     print_projects(&projects);
 }
--- src/project.rs
+++ src/project.rs
@@ -1,4 +1,7 @@
+use std::fs::{ReadDir, metadata, read_dir};
 use std::path::PathBuf;
+use std::io;
+use std::time::{SystemTime, Duration};
 use bytesize::ByteSize;
 
 use crate::project_type::ProjectType;
@@ -10,13 +13,15 @@ pub struct Project {
     pub rm_dirs: Vec<PathBuf>,
     pub rm_size: u64,
     pub rm_size_str: String,
+    pub last_modified: String,
 }
 
 impl Project {
     pub fn new(path: PathBuf, project_type: ProjectType, rm_dirs: Vec<PathBuf>) -> Project {
         let rm_size = get_rm_size(&path, &rm_dirs);
         let rm_size_str = ByteSize::b(rm_size as u64).to_string();
-        Project { path, project_type, rm_dirs, rm_size, rm_size_str }
+        let last_modified = get_time_since_last_mod(&path);
+        Project { path, project_type, rm_dirs, rm_size, rm_size_str, last_modified }
     }
 
     pub fn node(path: PathBuf) -> Project {
@@ -42,7 +47,8 @@ impl Project {
 
 pub fn print_projects(projects: &Vec<Project>) {
     const MIN_PADDING: usize = 10;
-    const PROJECT_TYPE_PADDING: usize = 10;
+    const PROJECT_TYPE_PADDING: usize = 8;
+    const LAST_MOD_PADDING: usize = 10;
     let mut max_path_len = 0;
     let mut max_size_len = 0;
 
@@ -55,20 +61,45 @@ pub fn print_projects(projects: &Vec<Project>) {
             max_size_len = project.rm_size_str.len();
         }
     }
-    println!("{}{}{}\n",
+    max_size_len += 2;
+    
+    println!("{}{}{}{}\n",
         format!("{:<width$}", "Path", width=(max_path_len + MIN_PADDING)),
         format!("{:<width$}", "Type", width=PROJECT_TYPE_PADDING),
-        format!("{:>width$}", "Size", width=max_size_len));
-    print!("{}{}{}\n",
+        format!("{:>width$}", "Last Mod.", width=LAST_MOD_PADDING),
+        format!("{:>width$}", "Size", width=max_size_len),
+    );
+    print!("{}{}{}{}\n",
         format!("{:<width$}", "----", width=(max_path_len + MIN_PADDING)),
         format!("{:<width$}", "----", width=PROJECT_TYPE_PADDING),
-        format!("{:>width$}", "----", width=max_size_len));
+        format!("{:>width$}", "----", width=LAST_MOD_PADDING),
+        format!("{:>width$}", "----", width=max_size_len),
+    );
     for project in projects {
-        println!("{}{}{}",
+        println!("{}{}{}{}",
             format!("{:<width$}", project.path.display(), width=(max_path_len + MIN_PADDING)),
             format!("{:<width$}", project.project_type.to_string(), width=PROJECT_TYPE_PADDING),
-            format!("{:>width$}", project.rm_size_str, width=max_size_len));
+            format!("{:>width$}", project.last_modified, width=LAST_MOD_PADDING),
+            format!("{:>width$}", project.rm_size_str, width=max_size_len),
+        );
     }
+}
+
+fn get_time_since_last_mod(path: &PathBuf) -> String {
+    const SECONDS_PER_DAY: u64 = 86400;
+    let meta = metadata(path);
+
+    if meta.is_err() { return String::from("Unknown"); }
+    let meta = meta.unwrap();
+    let last_mod = meta.modified();
+
+    if last_mod.is_err() { return String::from("Unknown"); }
+    let last_mod = last_mod.unwrap();
+    let time_since = SystemTime::now().duration_since(last_mod);
+
+    if time_since.is_err() { return String::from("Unknown"); }
+    let time_since_days = time_since.unwrap().as_secs() / Duration::from_secs(SECONDS_PER_DAY).as_secs();
+    format!("{} days", time_since_days)
 }
 
 fn get_rm_size(path: &PathBuf, rm_dirs: &Vec<PathBuf>) -> u64 {
@@ -77,10 +108,25 @@ fn get_rm_size(path: &PathBuf, rm_dirs: &Vec<PathBuf>) -> u64 {
         let path_exists = path.join(dir).try_exists();
 
         if path_exists.is_err() { continue; /* handle error */ }
-        let metadata = path.metadata();
+        let dir_size = dir_size(path.join(dir));
+        if dir_size.is_err() { continue; /* handle error */ }
 
-        if metadata.is_err() { continue; /* handle error */ }
-        size += metadata.unwrap().len();
+        size += dir_size.unwrap();
     }
     size
+}
+
+fn dir_size(path: PathBuf) -> io::Result<u64> {
+    fn dir_size(mut dir: ReadDir) -> io::Result<u64> {
+        dir.try_fold(0, |acc, file| {
+            let file = file?;
+            let size = match file.metadata()? {
+                data if data.is_dir() => dir_size(read_dir(file.path())?)?,
+                data => data.len(),
+            };
+            Ok(acc + size)
+        })
+    }
+
+    dir_size(read_dir(path)?)
 }
