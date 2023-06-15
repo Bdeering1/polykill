26f6e6b5577ff2a4243975e693e31e50780eeab8
Bryn Deering <bdeering@uoguelph.ca>
2023-06-15 15:28:32 -0400

    Fixed issue with conflicting ansi escape sequences for selected item

--- src/menu.rs
+++ src/menu.rs
@@ -44,14 +44,14 @@ fn create_label(project: &Project, max_path_len: usize) -> String {
     } else {
         project.project_type.to_string()
     };
-    let type_style = match project.project_type {
-        ProjectType::Cargo => Style::new().color256(214),
-        ProjectType::Node => Style::new().color256(28),
-        ProjectType::Dotnet => Style::new().color256(171),
-        ProjectType::Mix => Style::new().color256(98),
-        ProjectType::Gradle => Style::new().color256(42),
-        ProjectType::Composer => Style::new().color256(117),
-        ProjectType::Misc => Style::new().color256(147),
+    let type_color = match project.project_type {
+        ProjectType::Cargo => 214,
+        ProjectType::Node => 28,
+        ProjectType::Dotnet => 171,
+        ProjectType::Mix => 98,
+        ProjectType::Gradle => 42,
+        ProjectType::Composer => 117,
+        ProjectType::Misc => 147,
     };
 
     let last_modified = if let Some(days) = project.last_modified {
@@ -59,18 +59,18 @@ fn create_label(project: &Project, max_path_len: usize) -> String {
     } else {
         String::from("unknown")
     };
-    let last_mod_style = match project.last_modified {
-        Some(days) if days > 180 => Style::new().red(),
-        Some(days) if days > 30 => Style::new().yellow(),
-        Some(_days) => Style::new().green(),
-        None => Style::new().red(),
+    let last_mod_color = match project.last_modified {
+        Some(days) if days > 180 => 1,//Style::new().red(),
+        Some(days) if days > 30 => 3,//Style::new().yellow(),
+        Some(_days) => 2,//Style::new().green(),
+        None => 1,//Style::new().red(),
     };
 
     format!(
         "{}{}{}{}",
         format_args!("{:<width$}", project.path.display(), width = (max_path_len + MIN_PATH_PADDING)),
-        format_args!("{:<width$}", type_style.apply_to(project_type), width = PROJECT_TYPE_PADDING),
-        format_args!("{:>width$}", last_mod_style.apply_to(last_modified), width = LAST_MOD_PADDING),
+        format_args!("\x1b[38;5;{}m{:<width$}\x1b[39m", type_color, project_type, width = PROJECT_TYPE_PADDING),
+        format_args!("\x1b[38;5;{}m{:>width$}\x1b[39m", last_mod_color, last_modified, width = LAST_MOD_PADDING),
         format_args!("{:>width$}", project.rm_size_str, width = SIZE_PADDING)
     )
 }
