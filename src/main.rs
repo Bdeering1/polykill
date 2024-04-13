2098b88575efb4bd72a950ac9d984e0c05608d6c
Bryn Deering <bdeering@uoguelph.ca>
2024-04-13 14:45:48 -0400

    Switched out clap for bpaf

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,47 @@
 use std::{path::Path, cmp::Reverse};
-use clap::Parser;
+use bpaf::Bpaf;
 use console::Term;
 
 mod menu;
 mod project;
 mod search;
 
-#[derive(Debug, Parser)]
-#[clap(author, version, verbatim_doc_comment)]
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options)]
 /// Remove unwanted dependencies and build artifacts from local projects
-pub struct PolykillArgs {
-    #[clap(default_value_t = String::from("."))]
-    /// Directory to search for projects
-    pub dir: String,
-
+pub struct Options {
     /// Verbose output
-    #[arg(short, long)]
+    #[bpaf(short, long)]
     pub verbose: bool,
 
     /// Include projects not tracked by supported version control systems
-    #[arg(long)]
+    #[bpaf(long)]
     pub no_vcs: bool,
 
     /// Don't sort indexed projects
-    #[arg(short, long)]
+    #[bpaf(short, long)]
     pub unsorted: bool,
 
     /// Hide projects with zero possible disk savings
-    #[arg(short, long)]
+    #[bpaf(short, long)]
     pub skip_empty: bool,
 
     /// Don't bring up project menu (for testing purposes only)
-    #[arg(long)]
+    #[bpaf(long)]
     pub dry_run: bool,
+
+    #[bpaf(positional("DIR"))]
+    /// Directory to search for projects [default: .]
+    pub dir: Option<String>,
 }
 
 fn main() {
     const MAX_SEARCH_DEPTH: u32 = 10; // only applies if --no-vcs flag is specified
 
-    let args = PolykillArgs::parse();
-    let path = Path::new(args.dir.as_str());
+    let args = options().run();
+
+    let path_str = &args.dir.unwrap_or(".".to_string());
+    let path = Path::new(path_str);
     if !path.exists() {
         println!("Path '{}' does not exist.", path.display());
         return;
